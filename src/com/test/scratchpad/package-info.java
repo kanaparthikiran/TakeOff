9b2dcf0d671e063910f7bdfc4188bbd2bcf4d372
Kiran Kanaparthi <kkanaparthi@rh.com>
2018-04-14 20:54:46 -0700

    Added Scracthpad for Practice

---
+++ src/com/test/scratchpad/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author kkanaparthi
+ * 
+ * This package is meant for Scratch Pad Purposes
+ * The code in this package is either repeated or used 
+ * already
+ * 
+ * The classes in this package are meant  for repeated 
+ * practice
+ * 
+ */
+package com.test.scratchpad;
+
+
