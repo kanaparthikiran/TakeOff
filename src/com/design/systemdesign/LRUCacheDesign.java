a6dcbe76a512828dfb5e3993353c450c3996d66b
Kiran Kanaparthi <kanaparthikiran@gmail.com>
2018-04-24 20:12:28 -0700

    Added LRU Cache Design Solution.

---
+++ src/com/design/systemdesign/LRUCacheDesign.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.design.systemdesign;
+
+/**
+ * @author Kiran Kanaparthi
+ *
+ */
+public class LRUCacheDesign {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
